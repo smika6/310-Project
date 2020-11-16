a931b0ae4627f0e7a94907179c92ffef07e3244c
smika6 <smika6@users.noreply.github.com>
2020-11-16 13:53:05 -0600

    Getting it working in my VS Code

--- bin/BankersAlgorithmDriver.java
+++ bin/BankersAlgorithmDriver.java
@@ -1,10 +1,9 @@
 
 public class BankersAlgorithmDriver {
 
-  public static void main(String[] args){
-    System.out.println("Hello Team!");
+  public static void main(String[] args) {
+
+    System.out.println("Yo");
   }
-
-
 
 }
