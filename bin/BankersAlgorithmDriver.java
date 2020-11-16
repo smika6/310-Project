3076aab46d915b35b5b6a217f507201f4555f427
smika6 <smika6@users.noreply.github.com>
2020-11-16 14:00:37 -0600

    Inputs for the simulation

--- bin/BankersAlgorithmDriver.java
+++ bin/BankersAlgorithmDriver.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class BankersAlgorithmDriver {
 
   public static void main(String[] args) {
+    int recourceCount, threadCount;
 
-    System.out.println("Yo");
+    Scanner commandlineScanner = new Scanner(System.in);
+
+    System.out.print("Input the number of recource types: ");
+    recourceCount = commandlineScanner.nextInt();
+
+    System.out.print("Input the number of threads: ");
+    threadCount = commandlineScanner.nextInt();
+
+    String displayInputs = String.format("%nRunning Simulation with %s recources and %s threads.", recourceCount, threadCount);
+    System.out.println(displayInputs);
+
+    
   }
 
 }
