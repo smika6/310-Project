4706003ed93e3a3e369521bd7fe3b0660107f959
smika6 <smika6@users.noreply.github.com>
2020-11-16 14:11:05 -0600

    Finished the input of variables for simulation

--- bin/BankersAlgorithmDriver.java
+++ bin/BankersAlgorithmDriver.java
@@ -3,20 +3,41 @@ import java.util.Scanner;
 public class BankersAlgorithmDriver {
 
   public static void main(String[] args) {
-    int recourceCount, threadCount;
+    //primitives
+    int lowerRange = 1;
+    int upperRange = 10;
 
-    Scanner commandlineScanner = new Scanner(System.in);
+    int recourceCount = -1;
+    int threadCount = -1;
 
-    System.out.print("Input the number of recource types: ");
-    recourceCount = commandlineScanner.nextInt();
-
-    System.out.print("Input the number of threads: ");
-    threadCount = commandlineScanner.nextInt();
-
-    String displayInputs = String.format("%nRunning Simulation with %s recources and %s threads.", recourceCount, threadCount);
-    System.out.println(displayInputs);
-
+    //objects
+    String recourceCountRequestString = String.format("Input the number of recource types (%s-%s): ", lowerRange, upperRange);
+    String threadCountRequestString = String.format("Input the number of threads (%s-%s): ", lowerRange, upperRange);
     
+    Scanner commandlineScanner = new Scanner(System.in);
+    
+    //read in the count of resources, ensuring valid input
+    while (recourceCount < lowerRange || recourceCount > upperRange) {
+      System.out.print(recourceCountRequestString);
+      recourceCount = commandlineScanner.nextInt();
+    }
+    
+    System.out.println();
+
+    //read in the count of threads, ensuring valid input
+    while (threadCount < lowerRange || threadCount > upperRange) {
+      System.out.print(threadCountRequestString);
+      threadCount = commandlineScanner.nextInt();
+    }
+
+    //release recource
+    commandlineScanner.close();
+
+    //alert user of inputs provided.
+    String displayInputsString = String.format("%nRunning Simulation with %s recource(s) and %s thread(s).", recourceCount,
+        threadCount);
+    System.out.println(displayInputsString);
+
   }
 
 }
